bb046605fc56f6275f77504e4d9b667e1b06ec78
Erika <e.m.zibelnik@gmail.com>
2020-11-15 14:13:07 -0500

    trying to sort through errors with askbcs

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/navbar";
+import Home from "./pages/Home";
+import Login from "./pages/login";
+import Footer from "./components/Footer/footer";
+
 
 
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,64 +1,64 @@
 
-// import React from 'react';
-// import { Container, Row, Col, Card, CardBody, Button, CardTitle, CardText, CardImg, CardImgOverlay  } from 'reactstrap';
+import React from 'react';
+import { Container, Row, Col, Card, CardBody, Button, CardTitle, CardText, CardImg, CardImgOverlay  } from 'reactstrap';
 
-// function Home() {
-//   return (
-//     <Container>
-//       <Row xs="2">
-//         <Col>
-//         <Card>
-//         <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-//         <CardBody>
-//           <CardTitle tag="h5">Card Title</CardTitle>
-//           <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-//           <CardText>
-//             <small className="text-muted">Last updated 3 mins ago</small>
-//           </CardText>
-//         </CardBody>
-//       </Card>
-//         </Col>
-//         <Col>
-//         <Card inverse>
-//         <CardImg width="100%" src="/assets/318x270.svg" alt="Card image cap" />
-//         <CardImgOverlay>
-//           <CardTitle tag="h5">Card Title</CardTitle>
-//           <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-//           <CardText>
-//             <small className="text-muted">Last updated 3 mins ago</small>
-//           </CardText>
-//         </CardImgOverlay>
-//       </Card>
-//         </Col>
-//       </Row>
-//       <Row xs="3">
-//         <Col>
-//         <Card inverse>
-//         <CardImg width="100%" src="/assets/318x270.svg" alt="Card image cap" />
-//         <CardImgOverlay>
-//           <CardTitle tag="h5">Card Title</CardTitle>
-//           <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-//           <CardText>
-//             <small className="text-muted">Last updated 3 mins ago</small>
-//           </CardText>
-//         </CardImgOverlay>
-//       </Card>
-//         </Col>
-//         <Col>
-//         <Card>
-//         <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-//         <CardBody>
-//           <CardTitle tag="h5">Card Title</CardTitle>
-//           <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-//           <CardText>
-//             <small className="text-muted">Last updated 3 mins ago</small>
-//           </CardText>
-//         </CardBody>
-//       </Card>
-//         </Col>
-//         </Row>
-//     </Container>
-//   );
-// }
+function Home() {
+  return (
+    <Container>
+      <Row xs="2">
+        <Col>
+        <Card>
+        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle tag="h5">Card Title</CardTitle>
+          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+          <CardText>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </CardText>
+        </CardBody>
+      </Card>
+        </Col>
+        <Col>
+        <Card inverse>
+        <CardImg width="100%" src="/assets/318x270.svg" alt="Card image cap" />
+        <CardImgOverlay>
+          <CardTitle tag="h5">Card Title</CardTitle>
+          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+          <CardText>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </CardText>
+        </CardImgOverlay>
+      </Card>
+        </Col>
+      </Row>
+      <Row xs="3">
+        <Col>
+        <Card inverse>
+        <CardImg width="100%" src="/assets/318x270.svg" alt="Card image cap" />
+        <CardImgOverlay>
+          <CardTitle tag="h5">Card Title</CardTitle>
+          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+          <CardText>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </CardText>
+        </CardImgOverlay>
+      </Card>
+        </Col>
+        <Col>
+        <Card>
+        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle tag="h5">Card Title</CardTitle>
+          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+          <CardText>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </CardText>
+        </CardBody>
+      </Card>
+        </Col>
+        </Row>
+    </Container>
+  );
+}
 
-// export default Home;
+export default Home;
