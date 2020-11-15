10466bf1ca27afe869f99ac1747eb4987ab55c5f
Erika <e.m.zibelnik@gmail.com>
2020-11-15 14:33:34 -0500

    sorting out error codes, suggested restart

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/navbar";
 import Home from "./pages/Home";
-import Login from "./pages/login";
-import Footer from "./components/Footer/footer";
 
 
-
-
-class App extends Component {
-  render() {
+function App() {
     return (
       <Router>
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/Login" component={Login} />
         </Switch>
-        <Footer />
       </Router>
     );
   }
-}
+
 
 export default App;
