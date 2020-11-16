6d481f7a304361ab84022a40848f7cb8b95cf73b
tintin92 <tanner@schenck.xyz>
2020-11-16 11:48:17 -0500

    replaced path with routes, we may need both so i just commented it out.

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+// const path = require("path");
+const routes = required("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/animalsDB",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
 // Send every request to the React app
 // Define any API routes before this runs
