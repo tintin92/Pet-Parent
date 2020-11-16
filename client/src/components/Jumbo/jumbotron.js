e9436293925bc0c9e1769e70cd6e77a05bf22564
Erika <e.m.zibelnik@gmail.com>
2020-11-15 20:12:08 -0500

    added some css to jumbotron

--- client/src/components/Jumbo/jumbotron.js
+++ client/src/components/Jumbo/jumbotron.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Jumbotron, Button } from 'reactstrap';
+import "./style.css";
 
 function Jumbo() {
   return (
     <div>
       <Jumbotron>
-        <h1 className="display-3">Hello, Pet Pals!</h1>
+        <h1 className="display-3">Welcome to Pet Pals!</h1>
         <p className="lead">Start tracking your pet with this nifty web app today!</p>
         <hr className="my-2" />
-        <p>With a budget tracker and day to day tracker, you can learn more about your pet's needs.</p>
+        <p>With a budget tracker and day to day tracker -- you can learn more about your pet's needs.</p>
         <p className="lead">
-          <Button color="primary">Sign up here!</Button>
+          <Button className="btn" color="primary">Sign up here!</Button>
         </p>
       </Jumbotron>
     </div>
