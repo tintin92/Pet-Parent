5db6035401032541f67f01d3b688e9769d3f8f83
Erika <e.m.zibelnik@gmail.com>
2020-11-18 14:00:37 -0500

    working on paths - index.js (build instead of public)

--- client/src/App.js
+++ client/src/App.js
@@ -14,24 +14,24 @@ import { StoreProvider } from "./components/utils/GlobalState";
 
 function App() {
 
-    return (
-      <Router>
-        <div>
+  return (
+    <Router>
+      <div>
         <StoreProvider>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/createProfile" component={createProfile} />
-          <Route exact path="/Profile" component={Profile} />
-          <Route exact path="/Login" component={Login} />
-          <Route exact path="/Signup" component={Signup} />
-        </Switch>
-        <Footer />
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/createProfile" component={createProfile} />
+            <Route exact path="/Profile" component={Profile} />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/Signup" component={Signup} />
+          </Switch>
+          <Footer />
         </StoreProvider>
-        </div>
-      </Router>
-    );
-  }
+      </div>
+    </Router>
+  );
+}
 
 
 export default App;
